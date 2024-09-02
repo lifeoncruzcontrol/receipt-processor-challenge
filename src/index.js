3f445b59695492099d2b706739877edf457915ab
Chris <me@lifeoncruzcontrol.com>
2024-09-01 17:18:02 -0700

    Connected router to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ const Database = require('./database');
 const app = express();
 const port = 3000;
 
+const receiptsRoutes = require('./routes/receiptsRoutes');
+
+app.use(express.json());
+
 const db = new Database();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/receipts', receiptsRoutes);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
