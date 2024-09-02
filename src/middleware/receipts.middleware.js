a8938a58f99dd6867e81ce3534845ef52185d181
Chris <me@lifeoncruzcontrol.com>
2024-09-02 14:16:12 -0700

    Added regex to ensure date is in YYYY-MM-DD format

--- src/middleware/receipts.middleware.js
+++ src/middleware/receipts.middleware.js
@@ -2,10 +2,17 @@ const { body } = require('express-validator');
 
 const validateReceipt = [
   body('retailer').isString(),
-  body('purchaseDate').isISO8601().withMessage('Invalid date format'),
+
+  body('purchaseDate')
+    .matches(/^\d{4}-\d{2}-\d{2}$/).withMessage('Invalid date format. Expected yyyy-mm-dd')
+    .isISO8601().withMessage('Invalid ISO date format'),
+
   body('purchaseTime').matches(/^([01]\d|2[0-3]):([0-5]\d)$/).withMessage('Invalid time format'),
+
   body('items').isArray().withMessage('Items must be an array'),
+
   body('items.*.price').isString().matches(/^\d+(\.\d{2})?$/).withMessage('Price must be a valid decimal'),
+
   body('total').isString().matches(/^\d+(\.\d{2})?$/).withMessage('Total must be a valid decimal')
 ];
 
