039fa67904da5461b80dda707d99b4158c745844
Chris <me@lifeoncruzcontrol.com>
2024-09-01 17:51:23 -0700

    Removed database import from index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 // app.js
 const express = require('express');
-const Database = require('./database');
 const app = express();
 const port = 3000;
 
 const receiptsRoutes = require('./routes/receiptsRoutes');
 
 app.use(express.json());
-
-const db = new Database();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
