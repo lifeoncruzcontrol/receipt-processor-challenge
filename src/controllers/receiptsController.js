b569628ddbc113761f96a27f9cc88aea84c7ec77
Chris <me@lifeoncruzcontrol.com>
2024-09-01 19:54:22 -0700

    Added logic for last three rules

--- src/controllers/receiptsController.js
+++ src/controllers/receiptsController.js
@@ -18,6 +18,11 @@ const processReceipt = async (req, res) => {
 	}
 };
 
+const timeToMinutes = (time) => {
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
+}
+
 const getPoints = async (req, res) => {
 	try {
 		let points = 0;
@@ -41,6 +46,28 @@ const getPoints = async (req, res) => {
 		points += 5 * Math.floor(numItems / 2);
 
 		// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+		items.forEach(item => {
+			// remove all whitespace from description
+			const trimmed = item.shortDescription.trim();
+			if (trimmed.length % 3 === 0) {
+				points += Math.ceil(item.price * 0.2)
+			}
+		});
+
+		// 6 points if the day in the purchase date is odd.
+		const day = receipt.purchaseDate.split('-')[2];
+		if (day % 2 != 0) {
+			points += 6
+		}
+
+		// 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+		const time = timeToMinutes(receipt.purchaseTime);
+		const start = timeToMinutes("14:00");
+		const end = timeToMinutes("16:00");
+		
+		if (time >= start && time <= end) {
+			points += 10;
+		}
 
 		res.status(200).send({ 'points': points.toFixed(2) });	
 	} catch (err) {
