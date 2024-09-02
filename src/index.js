02c2f1e88672c12cfa673fa05469f8b8841ba012
Chris <me@lifeoncruzcontrol.com>
2024-09-01 18:24:43 -0700

    Passing middleware into route for validation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 const receiptsRoutes = require('./routes/receiptsRoutes');
+const validateReceipt = require('./middleware/receipts.middleware');
 
 app.use(express.json());
 
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use('/receipts', receiptsRoutes);
+app.use('/receipts', validateReceipt, receiptsRoutes);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
